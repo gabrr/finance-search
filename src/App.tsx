13c02fbe2d2980a0119265ce344d44d039c6dd02
Gabriel Oliveira <g.webdevelopr@gmail.com>
2024-02-25 22:44:07 -0300

    chore: cleaning app code

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,14 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useEffect } from "react";
-import { Bounce, ToastContainer, toast } from "react-toastify";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Bounce, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "tailwindcss/tailwind.css";
-import { Animations } from "./components/molecules/animations";
+import queryClient from "./services/reactQuery";
 import { Routes } from "./utils";
 
-const queryClient = new QueryClient();
-
-type SearchResult = {
-  id: string;
-  title: string;
-  url: string;
-  description: string;
-  category: "VIDEOS" | "PLAYLISTS" | "BLOG_POSTS";
-};
-
 function App() {
-  useEffect(() => {
-    toast("loaded");
-  }, []);
-
   return (
     <QueryClientProvider client={queryClient}>
       <Routes />
-
-      {/*<Animations.NotFoundError />
-      <Animations.Loading />
-      <Animations.NetworkError />
-      <Animations.EmptyState />*/}
 
       <ToastContainer
         position="top-right"
